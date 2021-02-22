66702a187f126dade18616e0288e73131b62016f
brandonchen2000 <66689033+brandonchen2000@users.noreply.github.com>
2021-02-22 15:40:12 -0500

    Update test_classify_stacked.py

--- test_classify_stacked.py
+++ test_classify_stacked.py
@@ -4,6 +4,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import classification_report, f1_score
 from sklearn.model_selection import KFold
 from sklearn.linear_model import LogisticRegression
+from sklearn.experimental import enable_hist_gradient_boosting
+from sklearn.ensemble import AdaBoostClassifier, HistGradientBoostingClassifier
 
 # Load training data file
 rdf = pd.read_csv('Train.csv')
@@ -11,7 +13,12 @@ rdf = pd.read_csv('Train.csv')
 # Remove rows without required features
 rdf = rdf[rdf['reviewText'].notna()]
 rdf = rdf[rdf['summary'].notna()]
+rdf = rdf[rdf['salesRank'].notna()]
 rdf = rdf[rdf['overall'].notna()]
+rdf = rdf[rdf['artist'].notna()]
+rdf = rdf[rdf['label'].notna()]
+
+rdf['label'] = rdf['label'].map(lambda x: hash(x) % (10 ** 8))
 
 # A product is awesome if its average overall rating is greater than 4.5 stars
 product_is_awesome = lambda x: 1 if np.mean(x) > 4.5 else 0
@@ -61,15 +68,28 @@ for train_idx, test_idx in kf.split(proddf):
     # Now we have actual and predicted review awesomeness scores (1: 5-star and 0: not 5-star).
     # Average the review scores to get a single value from 0 to 1.
     trainproddf['averageReviewScore'] = traindf.groupby('amazon-id').agg({'awesome': 'mean'})
+    testproddf['averageReviewScore'] = testdf.groupby('amazon-id').agg({'awesome': 'mean'})
     
     # FOR BRANDON - trainproddf and testproddf have artist, label, salesRank, and averageSAScore
     # columns as features, and the overall column as ground truth
 
-    print(trainproddf)
-    print(testproddf)
+    
+    # Prepare sentiment analysis data
+    X_prodtrain = trainproddf[['salesRank','artist','label','averageReviewScore']]
+    X_prodtest = testproddf[['salesRank','artist','label','averageReviewScore']]
+    y_prodtrain = trainproddf['overall'].values.reshape(-1,1)
+    y_prodtest = testproddf['overall'].values.reshape(-1,1)
 
-    print(classification_report(testproddf['overall'], prodpreddf['prediction']))
-    f1s.append(f1_score(testproddf['overall'], prodpreddf['prediction'], average='weighted'))
+    clf = AdaBoostClassifier(n_estimators=100)
+    clf.fit(X_prodtrain, y_prodtrain)
+    predictions = clf.predict(X_prodtest)
+    f1s.append(f1_score(y_prodtest, predictions, average='weighted'))
+
+    #print(trainproddf)
+    #print(testproddf)
+
+    print(classification_report(testproddf['overall'], predictions))
+    #f1s.append(f1_score(testproddf['overall'], prodpreddf['prediction'], average='weighted'))
 
 print(np.asarray(f1s).mean())
 
